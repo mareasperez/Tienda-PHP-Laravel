a1c2f7fe266f0b0ab67fe653a11b92b86ed2c747
Maury <mareasdperez7@gmail.com>
2020-09-11 06:24:29 -0600

    listo full

--- resources/views/products/lista.blade.php
+++ resources/views/products/lista.blade.php
@@ -1,52 +1,54 @@
 @extends('layout.main')
 @section('contenido')
-    <div class="container mt-3">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        Listado de Productos
-                        <a href="{{ route('products.create') }}" class="btn btn-success btn-sn float-right">Nuevo
-                            Producto</a>
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Listado de Productos
+                    <a href="{{ route('products.create') }}" class="btn btn-success btn-sn float-right">Nuevo
+                        Producto</a>
+                </div>
+                <div class="card-body">
+                    @if (session('info'))
+                    <div class="alert alert-success">
+                        {{ session('info') }}
                     </div>
-                    <div class="card-body">
-                        @if (session('info'))
-                            <div class="alert alert-success">
-                                {{ session('info') }}
-                            </div>
-                        @endif
-                        <table class="table table-hover table-sm">
-                            <thead>
-                                <th>Descripcion</th>
-                                <th>Precio</th>
-                                <th>Acciones</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($products as $product)
-                                    <tr>
-                                        <td> {{ $product->Description }}</td>
-                                        <td>{{ $product->price }}</td>
-                                        <td>
-                                            <a href="{{ route('products.edit', $product->id) }}"
-                                                class="btn btn-success">editar </a>
-                                            <a href="javascript: document.getElementById('delete-{{ $product->id }}').submit()"
-                                                class="btn btn-danger">Eliminar</a>
-                                            <form id="delete-{{ $product->id }}"
-                                                action="{{ route('products.delete', $product->id) }}" method="POST">
-                                                @method('delete')
-                                                @csrf
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
+                    @endif
+                    <table class="table table-hover table-sm">
+                        <thead>
+                            <th>Descripcion</th>
+                            <th>Precio</th>
+                            <th>Acciones</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                            <tr>
+                                <td> {{ $product->Description }}</td>
+                                <td>{{ $product->price }}</td>
+                                <td>
+                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-success">editar </a>
+                                    <a href="javascript: document.getElementById('delete-{{ $product->id }}').submit()" class="btn btn-danger">Eliminar</a>
+                                    <form id="delete-{{ $product->id }}" action="{{ route('products.delete', $product->id) }}" method="POST">
+                                        @method('delete')
+                                        @csrf
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    Bienvenido: {{auth()->user()->name}}
+                    <a class="btn btn-danger float-right" href="javascript: document.getElementById('logout').submit()"> cerrar sesion</a>
+                    <form action="{{route('logout')}}" id="logout" style="display:none" method="POST">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </body>
-</html>
 @endsection
